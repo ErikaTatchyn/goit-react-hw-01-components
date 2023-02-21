d6167d1e8a7964b90f16fa44f4fcbf3217525bb6
Erika Tatchyn <erika.ucu@gmail.com>
2023-02-21 13:05:15 +0000

    fix comments

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,37 +2,46 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './FriendList.module.css';
 
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusClass = isOnline ? styles.online : styles.offline;
+
+  return (
+    <li className="item">
+      <span className={statusClass}>{isOnline ? 'online' : 'offline'}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
 const FriendList = ({ friends }) => {
   return (
-    <div>
-      <h2>Friends</h2>
-      <ul className={styles.friendList}>
-        {friends.map(friend => (
-          <li key={friend.id} className={styles.item}>
-            <span className={friend.isOnline ? styles.online : styles.offline}>
-              {friend.isOnline ? 'online' : 'offline'}
-            </span>
-            <img
-              className={styles.avatar}
-              src={friend.avatar}
-              alt={friend.name}
-              width="48"
-            />
-            <p className={styles.name}>{friend.name}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
   );
 };
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
+      id: PropTypes.number.isRequired,
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
     })
   ).isRequired,
 };
